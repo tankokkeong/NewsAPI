7d06b32062cdb09d472a6271e1c3e7c4166b6eb4
Tan Kok Keong <tankokkeong4488@gmail.com>
2022-01-10 23:28:10 +0800

    Add project files.

---
+++ NewsAPI/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services;
+using NewsAPI;
+
+
+namespace NewsAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<JsonResult> SearchResults(string query, int page = 1)
+        {
+            var currentDate = DateTime.Now.ToString("yyyy-MM-dd");
+
+            var client = new HttpClient();
+
+            var url = "";
+
+            if(query == null || query == "")
+            {
+                url = "https://newsapi.org/v2/everything?q=Apple&from=" + currentDate + "&page=" + page + "&language=en&pageSize=20&apiKey=YOUR_API_KEY";
+            }
+            else
+            {
+               url = "https://newsapi.org/v2/everything?q=" + query + "&from=" + currentDate + "&page=" + page + "&language=en&pageSize=20&apiKey=YOUR_API_KEY";
+            }
+
+            var response = await client.GetAsync(url);
+
+            return  Json(response.Content.ReadAsStringAsync().Result, JsonRequestBehavior.AllowGet);
+
+        }
+
+
+    }
+}
---
+++ NewsAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewsAPI.MvcApplication" Language="C#" %>
